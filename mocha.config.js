512ac610572af10600475cc313c279d7c9556342
LFDM <1986gh@gmail.com>
2017-06-24 15:53:28 +0200

    fix(jsdom) Update setup to new syntax

--- mocha.config.js
+++ mocha.config.js
@@ -1,7 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import chai, { expect } from 'chai';
 import sinonChai from 'sinon-chai';
+import { JSDOM } from 'jsdom';
+import React from 'react'; // eslint-disable-line no-unused-vars
 
 chai.use(sinonChai);
+
+const getSelection = () => ({ baseNode: '' });
+
+global.window = new JSDOM('').window;
+global.document = global.window.document;
+global.document.getSelection = getSelection;
+global.window.getSelection = getSelection;
+
+global.navigator = { userAgent: 'browser', platform: 'Linux' };
 
 global.fdescribe = (...args) => describe.only(...args);
 global.fit = (...args) => it.only(...args);
