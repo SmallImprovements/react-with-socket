8ce3b8a90f46cd75f9b0b2a59a3240797a62b54b
LFDM <1986gh@gmail.com>
2017-06-24 15:32:55 +0200

    refactor(*) Make linter happy

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
 import { Component, createElement } from 'react';
 
 const config = {
-    base: '',
-    constructor: () => { throw new Error('No constructor defined'); },
+  base: '',
+  constructor: () => { throw new Error('No constructor defined'); }
 };
 
-export const setSocketBase = (base) => { config.base = base; }
-export const setSocketConstructor = (constructor) => { config.constructor = constructor; }
+export const setSocketBase = (base) => { config.base = base; };
+export const setSocketConstructor = (constructor) => { config.constructor = constructor; };
 export const setSocketConfig = (nextConfig) => Object.assign(config, nextConfig);
 
-const standardSocket = (url) => () => config.constructor(`${config.base}/${url}`)
+const standardSocket = (url) => () => config.constructor(`${config.base}/${url}`);
 const emptyMap = () => ({});
 const emptyActions = (emit) => ({ emit });
 
 const withSocket = (
-    createListeners = emptyMap,
-    createActions = emptyActions,
-    createCallbacks = emptyMap,
-    createSocket = standardSocket('')
+  createListeners = emptyMap,
+  createActions = emptyActions,
+  createCallbacks = emptyMap,
+  createSocket = standardSocket('')
 ) => (component) => {
-    class SocketWrapper extends Component {
-        constructor(props) {
-            super(props);
-            this.state = { props: {} };
-        }
-
-        componentWillMount() {
-            this.socket = createSocket();
-            const listeners = createListeners(this.props);
-            const callbacks = createCallbacks();
-
-            Object.keys(listeners).forEach((event) => {
-                this.socket.on(event, (data) => {
-
-                    const updater = (prevState) => {
-                        const nextProps = listeners[event](data, { ...this.props, ...prevState.props });
-                        return {
-                            ...prevState.state,
-                            props: {
-                                ...prevState.props,
-                                ...nextProps
-                            }
-                        };
-                    }
-
-                    const onUpdate = () => {
-                        const callback = callbacks[event];
-                        if (callback) {
-                            callback(data, this.state.props);
-                        }
-                    };
-
-                    this.setState(updater, onUpdate);
-                })
-            });
-        }
-
-        componentWillUnmount() {
-            this.socket.close();
-        }
-
-        render() {
-            const emit = (...args) => { this.socket.emit(...args); }
-            const nextProps = { ...this.props, ...this.state.props };
-            return createElement(component, { ...nextProps, ...createActions(emit, nextProps) });
-        }
+  class SocketWrapper extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { props: {} };
     }
 
-    return SocketWrapper;
-}
+    componentWillMount() {
+      this.socket = createSocket();
+      const listeners = createListeners(this.props);
+      const callbacks = createCallbacks();
+
+      Object.keys(listeners).forEach((event) => {
+        this.socket.on(event, (data) => {
+          const updater = (prevState) => {
+            const nextProps = listeners[event](data, { ...this.props, ...prevState.props });
+            return {
+              ...prevState.state,
+              props: {
+                ...prevState.props,
+                ...nextProps
+              }
+            };
+          };
+
+          const onUpdate = () => {
+            const callback = callbacks[event];
+            if (callback) {
+              callback(data, this.state.props);
+            }
+          };
+
+          this.setState(updater, onUpdate);
+        });
+      });
+    }
+
+    componentWillUnmount() {
+      this.socket.close();
+    }
+
+    render() {
+      const emit = (...args) => { this.socket.emit(...args); };
+      const nextProps = { ...this.props, ...this.state.props };
+      return createElement(component, { ...nextProps, ...createActions(emit, nextProps) });
+    }
+  }
+
+  return SocketWrapper;
+};
 
 export default withSocket;
